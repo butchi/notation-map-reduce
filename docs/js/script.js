07abc4230ac88f9c24e8f79b34afccdd4c2d9aff
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2017-07-08 01:53:00 +0900

    build

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,25 @@
-new NpmStarter();
+const notation = new Notation();
+
+console.log(notation.integerDigits(0, -2));
+
+console.log(notation.fromDigits([0, 1, 0, 1]));
+console.log(notation.fromDigits([0, 1, 0, 1], 10));
+console.log(notation.fromDigits([0, 1, 0, 1], 2));
+console.log(notation.fromDigits([1, 1, 1, 1, 1], -2));
+console.log(notation.fromDigits([1, 0, 1, 0, 1], notation.fibonacci));
+
+console.log('Negabinary:');
+
+notation.range(20).map(i => {
+  const v = notation.fromDigits(notation.integerDigits(i, -2), -2);
+  console.log(v);
+});
+
+console.log('Fibonacci Notation:');
+
+notation.range(20).map(i => {
+  const v = notation.integerDigits(i, 'fibonacci', 10);
+  console.log(v);
+});
+
+console.log('--');
